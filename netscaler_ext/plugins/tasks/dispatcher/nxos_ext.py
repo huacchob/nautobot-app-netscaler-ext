bae55f810344a81220806a9cce91e318fad402c2
Carlos Huaccho <huaccho.carlos0612@gmail.com>
2025-06-09 18:42:55 -0400

    adding command to nxos dispatcher

--- netscaler_ext/plugins/tasks/dispatcher/nxos_ext.py
+++ netscaler_ext/plugins/tasks/dispatcher/nxos_ext.py
@@ -1,7 +1,61 @@
 """nornir dispatcher for cisco NXOS."""
 
+from pathlib import Path
+
+import textfsm
 from nornir.core.task import Result, Task
 from nornir_nautobot.plugins.tasks.dispatcher.default import NetmikoDefault
+
+
+def snmp_user_template(snmp_user_output: str) -> list[dict[str, str]]:
+    """SNMP user textfsm template.
+
+    Args:
+        snmp_user_output (str): SNMP user command output.
+
+    Returns:
+        list[dict[str, str]]: List of parsed SNMP users.
+    """
+    file_path: Path = Path(__file__).parent
+
+    template_path: Path = file_path.joinpath(
+        "textfsm_templates/cisco_nxos_show_snmp_user.textfsm",
+    )
+    with open(file=template_path, mode="r", encoding="utf-8") as template_file:
+        fsm = textfsm.TextFSM(template=template_file)
+        parsed_results: list[str] = fsm.ParseText(text=snmp_user_output)
+
+    parsed_users: list[dict[str, str]] = []
+    for row in parsed_results:
+        parsed_users.append(dict(zip(fsm.header, row)))
+    return parsed_users
+
+
+def snmp_user_command_build(parsed_snmp_user: list[dict[str, str]]) -> str:
+    """Builds a list of SNMP user commands.
+
+    Args:
+        parsed_snmp_user (list[dict[str, str]]): List of parsed SNMP users.
+
+    Returns:
+        str: SNMP user commands.
+    """
+    snmp_user_commands: list[str] = []
+    for snmp_user in parsed_snmp_user:
+        single_user: str = ""
+        single_user += f"snmp-server user {snmp_user['USERNAME']} {snmp_user['GROUP']}"
+        if snmp_user["AUTH"]:
+            single_user += f" auth {snmp_user['AUTH']} <<<SNMP_USER_AUTH_KEY>>>"
+        if snmp_user["PRIV"]:
+            single_user += f" priv {snmp_user['PRIV']} <<<SNMP_USER_PRIV_KEY>>>"
+        single_user += " localizedkey"
+        snmp_user_commands.append(single_user)
+        if snmp_user["ACL_FILTER"]:
+            snmp_user_commands.append(
+                f"snmp-server user {snmp_user['USERNAME']} use-ipv4 acl {snmp_user['ACL_FILTER'][4:]}",
+            )
+
+    return "\n".join(snmp_user_commands)
 
 
 class NetmikoCiscoNxos(NetmikoDefault):
@@ -36,6 +90,16 @@ class NetmikoCiscoNxos(NetmikoDefault):
         full_config: str = ""
         for command in cls.config_commands:
             getter_result = cls.get_command(task, logger, obj, command)
+            if "snmp" in command:
+                snmp_user_result: list[dict[str, str]] = snmp_user_template(
+                    snmp_user_output=getter_result.result.get("output").get(
+                        command,
+                    ),
+                )
+                full_config += snmp_user_command_build(
+                    parsed_snmp_user=snmp_user_result,
+                )
+                continue
             full_config += getter_result.result.get("output").get(command)
         processed_config: str = cls._process_config(logger, full_config, remove_lines, substitute_lines, backup_file)
         return Result(host=task.host, result={"config": processed_config})
