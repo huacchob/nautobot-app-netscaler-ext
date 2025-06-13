a80d3150e17ab85f67ef1a6f0769764cc0837ced
Carlos Huaccho <huaccho.carlos0612@gmail.com>
2025-06-13 13:50:33 -0400

    adding logger to dispatcher

---
+++ netscaler_ext/plugins/tasks/dispatcher/meraki_dispatcher.py
@@ -0,0 +1,9 @@
+"""nornir dispatcher for cisco Meraki."""
+
+# import json
+# import os
+# import typing as t
+
+# from meraki import DashboardAPI
+# from nornir.core.task import Result, Task
+# from nornir_nautobot.plugins.tasks.dispatcher.default import NetmikoDefault
--- netscaler_ext/plugins/tasks/dispatcher/nxos_ext.py
+++ netscaler_ext/plugins/tasks/dispatcher/nxos_ext.py
@@ -99,6 +99,7 @@ class NetmikoCiscoNxos(NetmikoDefault):
                 { "config: <running configuration> }
         """
         logger.debug(f"Executing get_config for {task.host.name} on {task.host.platform}")
+        logger.info(obj.get_config_context())
         full_config: str = ""
         for command in cls.config_commands:
             getter_result = cls.get_command(task, logger, obj, command)
