3052ea468093912d8aa22d8a6fd14a3a15fc813b
Carlos Huaccho <huaccho.carlos0612@gmail.com>
2025-06-18 10:36:48 -0400

    creating meraki dispatcher

--- netscaler_ext/plugins/tasks/dispatcher/meraki_ext.py
+++ netscaler_ext/plugins/tasks/dispatcher/meraki_ext.py
@@ -39,7 +39,7 @@ def get_api_key(secrets_group: SecretsGroup) -> str:
     return api_key
 
 
-def open_dashboard_api(dash_url: str, dash_api_key: str) -> DashboardAPI:
+def open_dashboard_api(dashboard_url: str, dashboard_api_key: str) -> DashboardAPI:
     """Open Meraki Dashboard API.
 
     Args:
@@ -57,7 +57,7 @@ def open_dashboard_api(dash_url: str, dash_api_key: str) -> DashboardAPI:
     )
 
 
-def get_org_id(dash: DashboardAPI) -> str:
+def get_org_id(dashboard: DashboardAPI) -> str:
     """Get org ID from dashboard.
 
     Args:
@@ -66,8 +66,43 @@ def get_org_id(dash: DashboardAPI) -> str:
     Returns:
         str: Organization ID.
     """
-    orgs = dash.organizations.getOrganizations()
+    orgs = dashboard.organizations.getOrganizations()
     return orgs[0].get("id", "")
+
+
+def resolve_endpoint(
+    dashboard: DashboardAPI,
+    endpoint_context: list[dict[Any, Any]],
+    organizationId: str,
+    networkId: str,
+) -> Any:
+    """Resolve endpoint with parameters if any.
+
+    Args:
+        dashboard (DashboardAPI): _description_
+        endpoint_context (list[dict[Any, Any]]): _description_
+        organizationId (str): _description_
+        networkId (str): _description_
+
+    Returns:
+        Any: _description_
+    """
+    responses: list[dict[Any, Any]] = []
+    param_mapper: dict[str, str] = {
+        "organizationId": organizationId,
+        "networkId": networkId,
+    }
+    for endpoint in endpoint_context:
+        meraki_class, meraki_method = endpoint["method"].split(".")
+        class_callable = getattr(dashboard, meraki_class)
+        method_callable = getattr(class_callable, meraki_method)
+        params: dict[str, str] = {}
+        if endpoint.get("parameters"):
+            for param in endpoint["parameters"]:
+                params.update({param: param_mapper[param]})
+            responses.append(method_callable(**params))
+
+    return responses
 
 
 class MerakiDriver(NetmikoDefault):
@@ -91,14 +126,28 @@ class MerakiDriver(NetmikoDefault):
         api_key: str = get_api_key(secrets_group=obj.secrets_group)
         logger.info(f"key: {api_key}")
         dashboard: DashboardAPI = open_dashboard_api(
-            dash_url=dash_url,
-            dash_api_key=api_key,
+            dashboard_url=dash_url,
+            dashboard_api_key=api_key,
         )
-        org_id: str = get_org_id(dash=dashboard)
+        org_id: str = get_org_id(dashboard=dashboard)
         if not org_id:
             logger.error("Could not find the Meraki organization ID")
             raise ValueError("Could not find Meraki organization ID")
-        _running_config: list[dict[Any, Any]] = dashboard.organizations.getOrganizations()
+        feature_endpoints: str = cfg_cntx.get("endpoints", "")
+        if not feature_endpoints:
+            logger.error("Could not find the Meraki endpoints")
+            raise ValueError("Could not find Meraki endpoints")
+        _running_config: list[list[dict[Any, Any]]] = []
+        for feature in feature_endpoints:
+            endpoints: str = cfg_cntx.get(feature, "")
+            _running_config.append(
+                resolve_endpoint(
+                    dashboard=dashboard,
+                    endpoint_context=endpoints,
+                    organizationId=org_id,
+                    networkId="",
+                )
+            )
         processed_config: str = cls._process_config(
             logger=logger,
             running_config=str(_running_config),
