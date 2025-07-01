1535ed4f5413e3a9652c5a838e464ba19cad4808
Carlos Huaccho <huaccho.carlos0612@gmail.com>
2025-07-01 12:33:17 -0400

    implementing base class for controller drivers

---
+++ netscaler_ext/plugins/tasks/dispatcher/base_controller_driver.py
@@ -0,0 +1,156 @@
+"""Base nornir dispatcher for controllers."""
+
+import json
+from abc import ABC, abstractmethod
+from logging import Logger
+from typing import Any, Optional, OrderedDict
+
+from nautobot.dcim.models import Device
+from nornir.core.task import Result, Task
+from nornir_nautobot.plugins.tasks.dispatcher.default import NetmikoDefault
+
+
+class BaseControllerDriver(NetmikoDefault, ABC):
+    """Base Controller Dispatcher class."""
+
+    @classmethod
+    def _feature_name_parser(cls, feature_name: str) -> str:
+        """Feature name parser.
+
+        Args:
+            feature_name (str): The feature name from config context.
+
+        Returns:
+            str: Parsed feature name.
+        """
+        if "_" in feature_name:
+            feat = feature_name.rsplit(sep="_", maxsplit=1)[0]
+        elif "-" in feature_name:
+            feat = feature_name.rsplit(sep="-", maxsplit=1)[0]
+        else:
+            feat = feature_name.rsplit(sep=" ", maxsplit=1)[0]
+        return feat
+
+    @classmethod
+    @abstractmethod
+    def authenticate(
+        cls,
+        config_context: OrderedDict[Any, Any],
+        logger: Logger,
+        obj: Device,
+    ) -> Any:
+        """Authenticate to controller.
+
+        Args:
+            config_context (OrderedDict[Any, Any]): Config context.
+            logger (Logger): Logger object.
+            obj (Device): Device object.
+
+        Raises:
+            ValueError: Could not find the controller API URL in config context.
+
+        Returns:
+            Any: Controller object.
+        """
+        pass
+
+    @classmethod
+    @abstractmethod
+    def controller_setup(
+        cls,
+        controller_obj: Any,
+        logger: Logger,
+    ) -> dict[str, str]:
+        """Setup for controller.
+
+        Args:
+            controller_obj (Any): The controller object, i.e DashboardAPI for controller.
+            logger (Logger): Logger object.
+
+        Returns:
+            dict[str, str]: Map for controller data.
+        """
+        pass
+
+    @classmethod
+    @abstractmethod
+    def resolve_endpoint(
+        cls,
+        controller_obj: Any,
+        logger: Logger,
+        endpoint_context: list[dict[Any, Any]],
+        **kwargs: Any,
+    ) -> dict[str, dict[Any, Any]]:
+        """Resolve endpoint with parameters if any.
+
+        Args:
+            controller_obj (Any): Controller object.
+            logger (Logger): Logger object.
+            endpoint_context (list[dict[Any, Any]]): controller endpoint context.
+            kwargs (Any): Keyword arguments.
+
+        Returns:
+            Any: Dictionary of responses.
+        """
+        pass
+
+    @classmethod
+    def get_config(  # pylint: disable=R0913,R0914
+        cls,
+        task: Task,
+        logger: Logger,
+        obj: Device,
+        backup_file: str,
+        remove_lines: list[str],
+        substitute_lines: list[str],
+    ) -> Optional[Result]:
+        """Get the latest configuration from controller.
+
+        Args:
+            task (Task): Nornir Task.
+            logger (Logger): Nautobot logger.
+            obj (Device): Device object.
+            backup_file (str): Backup file location.
+            remove_lines (list[str]): Lines to remove from the configuration.
+            substitute_lines (list[str]): Lines to replace in the configuration.
+
+        Returns:
+            None | Result: Nornir Result object with a dict as a result
+                containing the running configuration or None.
+        """
+        cfg_cntx: OrderedDict[Any, Any] = obj.get_config_context()
+        controller_obj: Any = cls.authenticate(
+            config_context=cfg_cntx,
+            logger=logger,
+            obj=obj,
+        )
+        controller_dict: dict[str, str] = cls.controller_setup(
+            controller_obj=controller_obj,
+            logger=logger,
+        )
+        feature_endpoints: str = cfg_cntx.get("backup_endpoints", "")
+        if not feature_endpoints:
+            logger.error("Could not find the controller endpoints")
+            raise ValueError("Could not find controller endpoints")
+        _running_config: dict[str, dict[Any, Any]] = {}
+        for feature in feature_endpoints:
+            endpoints: list[dict[Any, Any]] = cfg_cntx.get(feature, "")
+            feature_name: str = _feature_name_parser(feature_name=feature)
+            _running_config.update(
+                {
+                    feature_name: cls.resolve_endpoint(
+                        controller_obj=controller_obj,
+                        logger=logger,
+                        endpoint_context=endpoints,
+                        **controller_dict,
+                    )
+                }
+            )
+        processed_config: str = cls._process_config(
+            logger=logger,
+            running_config=json.dumps(obj=_running_config, indent=4),
+            remove_lines=remove_lines,
+            substitute_lines=substitute_lines,
+            backup_file=backup_file,
+        )
+        return Result(host=task.host, result={"config": processed_config})
--- netscaler_ext/plugins/tasks/dispatcher/meraki_ext.py
+++ netscaler_ext/plugins/tasks/dispatcher/meraki_ext.py
@@ -4,7 +4,6 @@ import json
 from logging import Logger
 from typing import Any, Callable, Optional, OrderedDict
 
-import jmespath
 from meraki import DashboardAPI
 from nautobot.apps.choices import (
     SecretsGroupAccessTypeChoices,
@@ -13,11 +12,15 @@ from nautobot.apps.choices import (
 from nautobot.dcim.models import Device
 from nautobot.extras.models import SecretsGroup, SecretsGroupAssociation
 from nornir.core.task import Result, Task
-from nornir_nautobot.plugins.tasks.dispatcher.default import NetmikoDefault
+
+from netscaler_ext.plugins.tasks.dispatcher.base_controller_driver import (
+    BaseControllerDriver,
+)
 
 
-def get_api_key(secrets_group: SecretsGroup) -> str:
-    """Get Meraki Dashboard API Key.
+# Authentication private functions
+def _get_api_key(secrets_group: SecretsGroup) -> str:
+    """Get controller API Key.
 
     Args:
         secrets_group (SecretsGroup): SecretsGroup object.
@@ -43,152 +46,223 @@ def get_api_key(secrets_group: SecretsGroup) -> str:
     return api_key
 
 
-def open_dashboard_api(dashboard_url: str, dashboard_api_key: str) -> DashboardAPI:
-    """Open Meraki Dashboard API.
-
-    Args:
-        dashboard_url (str): Dashboard URL.
-        dashboard_api_key (str): Dashboard API key.
-
-    Returns:
-        DashboardAPI: Dashboard API object.
-    """
-    return DashboardAPI(
-        api_key=dashboard_api_key,
-        base_url=dashboard_url,
-        output_log=False,
-        print_console=False,
-    )
-
-
-def get_org_id(dashboard: DashboardAPI) -> str:
-    """Get org ID from dashboard.
-
-    Args:
-        dashboard (DashboardAPI): The DashboardAPI object.
-
-    Returns:
-        str: Organization ID.
-    """
-    return dashboard.organizations.getOrganizations()[0].get("id", "")
-
-
-def get_case_insensitive_key(
-    params_mapper: dict[str, str],
-    param: str,
-) -> tuple[str, str]:
-    """Get case insensitive key from params mapper dict.
-
-    Args:
-        params_mapper (dict[str, str]): Params mapper.
-        param (str): Param key.
-
-    Returns:
-        tuple[str, str]: Param key and value.
-    """
-    for k, v in params_mapper.items():
-        if k.lower() == param.lower():
-            return (k, v)
-    return ("", "")
-
-
-def feature_name_parser(feature_name: str) -> str:
-    """Feature name parser.
-
-    Args:
-        feature_name (str): The feature name from config context.
-
-    Returns:
-        str: Parsed feature name.
-    """
-    if "_" in feature_name:
-        feat = feature_name.rsplit(sep="_", maxsplit=1)[0]
-    elif "-" in feature_name:
-        feat = feature_name.rsplit(sep="-", maxsplit=1)[0]
-    else:
-        feat = feature_name.rsplit(sep=" ", maxsplit=1)[0]
-    return feat
-
-
-def resolve_endpoint(
-    dashboard: DashboardAPI,
+# Resolving endpoint private functions
+def _resolve_method_callable(
+    controller_obj: Any,
+    method: str,
     logger: Logger,
-    endpoint_context: list[dict[Any, Any]],
-    organizationId: str,
-    networkId: str,
-) -> dict[str, dict[Any, Any]]:
-    """Resolve endpoint with parameters if any.
+) -> Optional[Callable[[Any], Any]]:
+    """Resolve method callable.
 
     Args:
-        dashboard (DashboardAPI): Dashboard API object.
+        controller_obj (Any): Controller object, i.e. DashboardAPI for Meraki.
+        method (str): 'class.method' name.
         logger (Logger): Logger object.
-        endpoint_context (list[dict[Any, Any]]): Meraki endpoint context.
-        organizationId (str): Organization ID.
-        networkId (str): Network ID.
 
     Returns:
-        Any: Dictionary of responses.
+        Optional[Callable[[Any], Any]]: Method callable or None.
     """
-    responses: dict[str, dict[Any, Any]] = {}
-    param_mapper: dict[str, str] = {
-        "organizationId": organizationId,
-        "networkId": networkId,
-    }
-    for endpoint in endpoint_context:
-        meraki_class, meraki_method = endpoint["method"].split(".")
-        try:
-            class_callable: Callable[[Any], Any] = getattr(
-                dashboard,
-                meraki_class,
-            )
-        except AttributeError:
-            logger.error(
-                f"The class {meraki_class} does not exist in the Dashboard API",
-            )
-            continue
-        try:
-            method_callable: Callable[[Any], Any] = getattr(
-                class_callable,
-                meraki_method,
-            )
-        except AttributeError:
-            logger.error(
-                f"The method {meraki_method} does not exist in the {meraki_class} class",
-            )
-            continue
-        params: dict[str, str] = {}
-        if endpoint.get("parameters"):
-            for param in endpoint["parameters"]:
-                if param.lower() not in [p.lower() for p in param_mapper]:
-                    continue
-                param_key, param_value = get_case_insensitive_key(
-                    params_mapper=param_mapper,
-                    param=param,
-                )
-                params.update({param_key: param_value})
-        try:
-            response: dict[Any, Any] = method_callable(**params)
-        except TypeError as e:
-            logger.error(
-                f"The params {params} are not valid/sufficient for the {meraki_class}.{meraki_method} method",
-            )
-            logger.warning(
-                e,
-            )
-            continue
-        for jpath in endpoint["jmespath"]:
-            for key, value in jpath.items():
-                j_value = jmespath.search(
-                    expression=value,
-                    data=response,
-                )
-                responses.update({key: j_value})
-
-    return responses
+    cotroller_class, controller_method = method.split(sep=".")
+    try:
+        class_callable: Callable[[Any], Any] = getattr(
+            controller_obj,
+            cotroller_class,
+        )
+    except AttributeError:
+        logger.error(
+            f"The class {cotroller_class} does not exist in the controller object",
+        )
+        return
+    try:
+        method_callable: Callable[[Any], Any] = getattr(
+            class_callable,
+            controller_method,
+        )
+    except AttributeError:
+        logger.error(
+            f"The method {controller_method} does not exist in the {cotroller_class} class",
+        )
+        return
+    return method_callable
 
 
-class MerakiDriver(NetmikoDefault):
+def _resolve_params(
+    parameters: list[str],
+    param_mapper: dict[str, str],
+) -> dict[Any, Any]:
+    """Resolve parameters.
+
+    Args:
+        parameters (list[str]): Parameters list.
+        param_mapper (dict[str, str]): Parameters mapper.
+
+    Returns:
+        dict[Any, Any]: _description_
+    """
+    params: dict[Any, Any] = {}
+    if parameters:
+        for param in parameters:
+            if param.lower() not in [p.lower() for p in param_mapper]:
+                continue
+            param_key, param_value = get_case_insensitive_key(
+                params_mapper=param_mapper,
+                param=param,
+            )
+            for k, v in param_mapper.items():
+                if k.lower() == param.lower():
+                    param_key, param_value = k, v
+                else:
+                    param_key, param_value = "", ""
+            params.update({param_key: param_value})
+    return params
+
+
+def _resolve_jmespath(
+    jmespath: list[dict[str, str]],
+    api_response: Any,
+) -> dict[Any, Any]:
+    """Resolve jmespath.
+
+    Args:
+        jmespath (list[dict[str, str]]): Jmespath list.
+        api_response (Any): API response.
+
+    Returns:
+        dict[str, Any]: Resolved jmespath data fields.
+    """
+    data_fields: dict[str, Any] = {}
+    for jpath in jmespath:
+        for key, value in jpath.items():
+            j_value: Any = jmespath.search(
+                expression=value,
+                data=api_response,
+            )
+            data_fields.update({key: j_value})
+    return data_fields
+
+
+class MerakiDriver(BaseControllerDriver):
     """Meraki Dispatcher class."""
+
+    @classmethod
+    def authenticate(
+        cls,
+        config_context: OrderedDict[Any, Any],
+        logger: Logger,
+        obj: Device,
+    ) -> Any:
+        """Authenticate to controller.
+
+        Args:
+            config_context (OrderedDict[Any, Any]): Config context.
+            logger (Logger): Logger object.
+            obj (Device): Device object.
+
+        Raises:
+            ValueError: Could not find the controller API URL in config context.
+
+        Returns:
+            Any: Controller object.
+        """
+        controller_url: str = config_context.get("dashboard_url", "")
+        if not controller_url:
+            logger.error("Could not find the Meraki Dashboard API URL")
+            raise ValueError("Could not find the Meraki Dashboard API URL")
+        api_key: str = _get_api_key(secrets_group=obj.secrets_group)
+        controller_obj: DashboardAPI = DashboardAPI(
+            api_key=api_key,
+            base_url=controller_url,
+            output_log=False,
+            print_console=False,
+        )
+        return controller_obj
+
+    @classmethod
+    def controller_setup(
+        cls,
+        controller_obj: Any,
+        logger: Logger,
+    ) -> dict[str, str]:
+        """Setup for controller.
+
+        Args:
+            controller_obj (Any): The controller object, i.e DashboardAPI for Meraki.
+            logger (Logger): Logger object.
+
+        Returns:
+            dict[str, str]: Map for controller data.
+        """
+        org_id: str = controller_obj.organizations.getOrganizations()[0].get("id", "")
+        if not org_id:
+            logger.error("Could not find the Meraki organization ID")
+            raise ValueError("Could not find Meraki organization ID")
+        networkId = ""
+        return {
+            "organizationId": org_id,
+            "networkId": networkId,
+        }
+
+    @classmethod
+    def resolve_endpoint(
+        cls,
+        controller_obj: Any,
+        logger: Logger,
+        endpoint_context: list[dict[Any, Any]],
+        **kwargs: Any,
+    ) -> dict[str, dict[Any, Any]]:
+        """Resolve endpoint with parameters if any.
+
+        Args:
+            controller_obj (Any): Controller object.
+            logger (Logger): Logger object.
+            endpoint_context (list[dict[Any, Any]]): Meraki endpoint context.
+            kwargs (Any): Keyword arguments.
+
+        Returns:
+            Any: Dictionary of responses.
+        """
+        try:
+            organization_id: str = kwargs["organizationId"]
+            network_id: str = kwargs["networkId"]
+        except KeyError as exc:
+            missing: str = exc.args[0]
+            raise ValueError(
+                f"resolve_endpoint() needs '{missing}' in kwargs",
+            ) from exc
+        responses: dict[str, dict[Any, Any]] = {}
+        param_mapper: dict[str, str] = {
+            "organizationId": organization_id,
+            "networkId": network_id,
+        }
+        for endpoint in endpoint_context:
+            method_callable: Optional[Callable[[Any], Any]] = _resolve_method_callable(
+                controller_obj=controller_obj,
+                method=endpoint["method"],
+                logger=logger,
+            )
+            if not method_callable:
+                continue
+            params: dict[Any, Any] = _resolve_params(
+                parameters=endpoint.get("parameters"),
+                param_mapper=param_mapper,
+            )
+            try:
+                response: Any = method_callable(**params)
+            except TypeError as e:
+                logger.error(
+                    f"The params {params} are not valid/sufficient for the {method_callable} method",
+                )
+                logger.warning(
+                    e,
+                )
+                continue
+            jpath_fields: dict[str, Any] = _resolve_jmespath(
+                jmespath=endpoint["jmespath"],
+                api_response=response,
+            )
+            responses.update(jpath_fields)
+
+        return responses
 
     @classmethod
     def get_config(  # pylint: disable=R0913,R0914
@@ -200,7 +274,7 @@ class MerakiDriver(NetmikoDefault):
         remove_lines: list[str],
         substitute_lines: list[str],
     ) -> Optional[Result]:
-        """Get the latest configuration from Meraki dashboard.
+        """Get the latest configuration from controller.
 
         Args:
             task (Task): Nornir Task.
@@ -215,35 +289,30 @@ class MerakiDriver(NetmikoDefault):
                 containing the running configuration or None.
         """
         cfg_cntx: OrderedDict[Any, Any] = obj.get_config_context()
-        dash_url: str = cfg_cntx.get("dashboard_url", "")
-        if not dash_url:
-            logger.error("Could not find the Meraki Dashboard API URL")
-            raise ValueError("Could not find the Meraki Dashboard API URL")
-        api_key: str = get_api_key(secrets_group=obj.secrets_group)
-        dashboard: DashboardAPI = open_dashboard_api(
-            dashboard_url=dash_url,
-            dashboard_api_key=api_key,
+        controller_obj: DashboardAPI = cls.authenticate(
+            config_context=cfg_cntx,
+            logger=logger,
+            obj=obj,
         )
-        org_id: str = get_org_id(dashboard=dashboard)
-        if not org_id:
-            logger.error("Could not find the Meraki organization ID")
-            raise ValueError("Could not find Meraki organization ID")
+        controller_dict: dict[str, str] = cls.controller_setup(
+            controller_obj=controller_obj,
+            logger=logger,
+        )
         feature_endpoints: str = cfg_cntx.get("backup_endpoints", "")
         if not feature_endpoints:
             logger.error("Could not find the Meraki endpoints")
             raise ValueError("Could not find Meraki endpoints")
-        _running_config: list[dict[str, dict[Any, Any]]] = {}
+        _running_config: dict[str, dict[Any, Any]] = {}
         for feature in feature_endpoints:
             endpoints: list[dict[Any, Any]] = cfg_cntx.get(feature, "")
-            feature_name: str = feature_name_parser(feature_name=feature)
+            feature_name: str = _feature_name_parser(feature_name=feature)
             _running_config.update(
                 {
-                    feature_name: resolve_endpoint(
-                        dashboard=dashboard,
+                    feature_name: cls.resolve_endpoint(
+                        controller_obj=controller_obj,
                         logger=logger,
                         endpoint_context=endpoints,
-                        organizationId=org_id,
-                        networkId="",
+                        **controller_dict,
                     )
                 }
             )
