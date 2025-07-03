2bb6fe6b59bab16df57feab2cd57b4687626e9b1
Carlos Huaccho <huaccho.carlos0612@gmail.com>
2025-07-03 08:42:57 -0400

    adding argument support to remediation

--- netscaler_ext/plugins/tasks/remediation/controller_remediation.py
+++ netscaler_ext/plugins/tasks/remediation/controller_remediation.py
@@ -12,22 +12,30 @@ if TYPE_CHECKING:
     from nautobot_golden_config.models import ConfigCompliance
 
 
-def _feature_name_parser(feature_name: str) -> str:
-    """Feature name parser.
+def _filter_allowed_params(
+    feature_name: str,
+    config_context: list[dict[str, Any]],
+    config: dict[str, Any],
+) -> dict[str, Any]:
+    """Filter allowed parameters and remove unwanted parameters.
 
     Args:
-        feature_name (str): The feature name from config context.
+        feature_name (str): Compliance feature name.
+        config_context (ConfigContext): Device config context.
+        config (dict[str, Any]): Intended or actual config.
 
     Returns:
-        str: Parsed feature name.
+        dict[str, Any]: Filtered config.
     """
-    if "_" in feature_name:
-        feat = feature_name.rsplit(sep="_", maxsplit=1)[0]
-    elif "-" in feature_name:
-        feat = feature_name.rsplit(sep="-", maxsplit=1)[0]
-    else:
-        feat = feature_name.rsplit(sep=" ", maxsplit=1)[0]
-    return feat
+    valid_payload_config: dict[str, Any] = {feature_name: {}}
+    all_optional_arguments: list[str] = []
+    for endpoint in config_context:
+        all_optional_arguments.extend(endpoint["parameters"]["optional"])
+
+    for key, value in config[feature_name].items():
+        if key in all_optional_arguments:
+            valid_payload_config[feature_name][key] = value
+    return valid_payload_config
 
 
 def _process_diff(diff: Dict[Any, Any], path: Tuple[str, ...], value: Any) -> None:
@@ -56,9 +64,20 @@ def controller_remediation(obj: "ConfigCompliance") -> str:
     Returns:
         str: Remediation json config.
     """
+    feature_name: str = obj.rule.feature.name
+    intended: dict[str, Any] = _filter_allowed_params(
+        feature_name=feature_name,
+        config_context=obj.device.get_config_context().get(f"{feature_name}_remediation", ""),
+        config=obj.intended,
+    )
+    actual: dict[str, Any] = _filter_allowed_params(
+        feature_name=feature_name,
+        config_context=obj.device.get_config_context().get(f"{feature_name}_remediation", ""),
+        config=obj.actual,
+    )
     diff: Dict[str, Any] = {}
     stack: deque[Tuple[Tuple[str, ...], Any, Any]] = deque()
-    stack.append((tuple(), obj.actual, obj.intended))
+    stack.append((tuple(), actual, intended))
 
     while stack:
         path, actual, intended = stack.pop()
@@ -78,7 +97,6 @@ def controller_remediation(obj: "ConfigCompliance") -> str:
             if actual != intended:
                 _process_diff(diff=diff, path=path, value=intended)
 
-    parsed_feature_name: str = _feature_name_parser(feature_name=obj.rule.feature.name)
-    if not diff.get(parsed_feature_name.lower()):
-        raise ValidationError(f"Feature {parsed_feature_name} not found in diff.")
-    return json.dumps(diff[parsed_feature_name.lower()], indent=4)
+    if not diff.get(feature_name.lower()):
+        raise ValidationError(f"Feature {feature_name} not found in diff.")
+    return json.dumps(diff[feature_name.lower()], indent=4)
