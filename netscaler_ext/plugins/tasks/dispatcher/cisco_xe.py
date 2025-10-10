613b0c4f94b1152a0c461e5fbf4391160445ecca
huacchob <59075675+huacchob@users.noreply.github.com>
2025-10-10 19:57:06 -0400

    adding ios textfsm tenplate

--- netscaler_ext/plugins/tasks/dispatcher/cisco_xe.py
+++ netscaler_ext/plugins/tasks/dispatcher/cisco_xe.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import textfsm
 from nornir.core.task import Result, Task
 from nornir_nautobot.plugins.tasks.dispatcher.default import NetmikoDefault
+from remote_pdb import RemotePdb
 
 
 def snmp_user_template(snmp_user_output: str) -> list[dict[str, str]]:
@@ -41,6 +42,7 @@ def snmp_user_command_build(parsed_snmp_user: list[dict[str, str]]) -> str:
     Returns:
         str: SNMP user commands.
     """
+    RemotePdb(host="localhost", port=4444).set_trace()
     snmp_user_commands: list[str] = []
     if not parsed_snmp_user:
         return ""
